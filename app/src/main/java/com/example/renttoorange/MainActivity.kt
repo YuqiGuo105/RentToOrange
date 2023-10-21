b69a868a907218797591d40dd23bf54000f7ab27
YuqiGuo105 <131561736+YuqiGuo105@users.noreply.github.com>
2023-10-21 15:18:37 -0400

    Log In function
    1. If the user doesn't log in, the app will push user to log in. Otherwise, it will jump to homepage.
    
    2. The homepage will display username (get value from sharePreference).
    
    3. The log out button. Once click it, it will go back to log in page and then clear all sharePreference data.

--- app/src/main/java/com/example/renttoorange/MainActivity.kt
+++ app/src/main/java/com/example/renttoorange/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.renttoorange
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.renttoorange.view.Homepage
+import com.example.renttoorange.view.LogIn
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val username = getUsernameFromPreferences()
+
+        // If the username is null (user not logged in), redirect to LogIn activity
+        if (username == null) {
+            val intent = Intent(this, LogIn::class.java)
+            startActivity(intent)
+            finish() // This ensures that the MainActivity is not in the back stack after redirection
+            return
+        } else {
+            val intent = Intent(this, Homepage::class.java)
+            startActivity(intent)
+            finish() // This ensures that the MainActivity is not in the back stack after redirection
+            return
+        }
+    }
+
+    private fun getUsernameFromPreferences(): String? {
+        val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("username", null)
     }
 }
