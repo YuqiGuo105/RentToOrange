d22b607722c9978fa5fc78d94512d1ac44d69e26
YuqiGuo105 <131561736+YuqiGuo105@users.noreply.github.com>
2023-12-03 00:00:03 -0500

    Bug fix for category

--- app/src/main/java/com/example/renttoorange/dao/RentInfoRepository.kt
+++ app/src/main/java/com/example/renttoorange/dao/RentInfoRepository.kt
@@ -1,12 +1,9 @@
 package com.example.renttoorange.dao
 
+import android.util.Log
 import com.example.renttoorange.model.RentInfo
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.Query
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.getValue
 
 class RentInfoRepository {
@@ -24,26 +21,6 @@ class RentInfoRepository {
             }
         } ?: callback(false)
     }
-
-    fun getRentInfoByType(type: String, callback: (List<RentInfo>) -> Unit) {
-        val query = FirebaseDatabase.getInstance().reference.child("rentInfo")
-            .orderByChild("category")
-            .equalTo(type)
-
-        query.get().addOnSuccessListener { dataSnapshot ->
-            val rentInfoList = mutableListOf<RentInfo>()
-            for (item in dataSnapshot.children) {
-                item.getValue<RentInfo>()?.let { rentInfo ->
-                    rentInfoList.add(rentInfo)
-                }
-            }
-            callback(rentInfoList)
-        }.addOnFailureListener {
-            // Handle the failure case, e.g., by returning an empty list or logging the error
-            callback(emptyList())
-        }
-    }
-
 
     fun retrieveAllRentInfo(callback: (List<RentInfo>?) -> Unit) {
         database.get().addOnSuccessListener {
--- app/src/main/java/com/example/renttoorange/view/MainFragments/CategoryFragment.kt
+++ app/src/main/java/com/example/renttoorange/view/MainFragments/CategoryFragment.kt
@@ -1,12 +1,14 @@
 package com.example.renttoorange.view.MainFragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,6 +22,7 @@ import com.example.renttoorange.view.Adapters.RentInfoAdapter
 class CategoryFragment : Fragment() {
     private lateinit var rentInfoRecyclerView: RecyclerView
     private val rentInfoRepository = RentInfoRepository()
+    private var selectedCategory: HouseType? = null // null represents
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_category, container, false)
@@ -27,14 +30,35 @@ class CategoryFragment : Fragment() {
         rentInfoRecyclerView = view.findViewById(R.id.rentInfoRecyclerView)
         rentInfoRecyclerView.layoutManager = LinearLayoutManager(context)
 
+        // Initialize buttons and set click listeners
+        view.findViewById<Button>(R.id.all).setOnClickListener { changeCategory(null) }
+        view.findViewById<Button>(R.id.btnApartment).setOnClickListener { changeCategory(HouseType.APARTMENT) }
+        view.findViewById<Button>(R.id.btnHouse).setOnClickListener { changeCategory(HouseType.HOUSE) }
+
+
         rentInfoRepository.retrieveAllRentInfo { rentInfoList ->
-            if (rentInfoList != null) {
-                rentInfoRecyclerView.adapter = RentInfoAdapter(rentInfoList)
-            } else {
+            rentInfoList?.let {
+                val filteredList = it.filter { rentInfo -> rentInfo.category == selectedCategory }
+                rentInfoRecyclerView.adapter = RentInfoAdapter(filteredList)
+            } ?: run {
                 // Handle the error scenario
             }
         }
+
+
         return view
+    }
+
+    fun changeCategory(newCategory: HouseType?) {
+        selectedCategory = newCategory
+        rentInfoRepository.retrieveAllRentInfo { rentInfoList ->
+            rentInfoList?.let {
+                val filteredList = selectedCategory?.let { category ->
+                    it.filter { rentInfo -> rentInfo.category == category }
+                } ?: it // If 'null' (ALL), no filtering is applied
+                rentInfoRecyclerView.adapter = RentInfoAdapter(filteredList)
+            }
+        }
     }
 
 }
